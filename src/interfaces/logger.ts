fb8edd1ca207f5e0972f7282cb2cd16ca606ab66
Wim Haanstra <wim@x256.nl>
2024-09-10 10:15:29 +0200

    Include more log levels in `Logger` interface

--- src/interfaces/logger.ts
+++ src/interfaces/logger.ts
@@ -1,4 +1,7 @@
 export interface Logger {
-    info(message?: any, ...messages: any[]): void;
-    error(message?: any, ...messages: any[]): void;
+  trace(message?: any, ...messages: any[]): void;
+  debug(message?: any, ...messages: any[]): void;
+  info(message?: any, ...messages: any[]): void;
+  warn(message?: any, ...messages: any[]): void;
+  fatal(message?: any, ...messages: any[]): void;
 }
