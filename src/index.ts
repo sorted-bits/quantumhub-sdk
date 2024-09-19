3b7a23482ab589960919c75468f27e6e4784b913
Wim Haanstra <wim@x256.nl>
2024-09-19 19:31:30 +0200

    Export devices

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,14 @@ export { Logger } from './interfaces/logger';
 export { PackageConfig } from './interfaces/package-config';
 export { Provider } from './interfaces/provider';
 
+export { BaseDevice } from './interfaces/devices/base-device';
+export { ButtonDevice } from './interfaces/devices/button-device';
+export { ClimateDevice } from './interfaces/devices/climate-device';
+export { NumberDevice } from './interfaces/devices/number-device';
+export { SceneDevice } from './interfaces/devices/scene-device';
+export { SelectDevice } from './interfaces/devices/select-device';
+export { SwitchDevice } from './interfaces/devices/switch-device';
+
 export { Attribute, ButtonAttribute, DeviceAutomationAttribute, NumberAttribute, SceneAttribute, SelectAttribute, SwitchAttribute } from './interfaces/attributes/device-attributes';
 
 export { DeviceClass } from './enums/device-class';
