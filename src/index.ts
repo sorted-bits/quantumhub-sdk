e4c3ac5c3cff370d9e880f47e338dbb8c3790682
Wim Haanstra <wim@x256.nl>
2024-09-20 06:50:58 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,9 @@ export { Logger } from './interfaces/logger';
 export { PackageConfig } from './interfaces/package-config';
 export { Provider } from './interfaces/provider';
 
-export { Attribute, ButtonAttribute, DeviceAutomationAttribute, NumberAttribute, SceneAttribute, SelectAttribute, SwitchAttribute } from './interfaces/attributes/device-attributes';
+export * from './interfaces/attributes/device-attributes';
+
+// export { Attribute, ButtonAttribute, ClimateAttribute, DeviceAutomationAttribute, NumberAttribute, SceneAttribute, SelectAttribute, SwitchAttribute } from './interfaces/attributes/device-attributes';
 
 export { DeviceClass } from './enums/device-class';
 export { DeviceType } from './enums/device-type';
