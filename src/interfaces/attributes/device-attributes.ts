dcc22b3787150881afc630688323f657c4d8de3d
Wim Haanstra <wim@haanstra.io>
2024-10-09 15:42:09 +0200

    Light attributes moved

--- src/interfaces/attributes/device-attributes.ts
+++ src/interfaces/attributes/device-attributes.ts
@@ -9,8 +9,44 @@ export interface SceneAttribute extends BaseAttribute {
   type: DeviceType.scene;
 }
 
+// JSON Schema for Light MQTT Configuration
+// https://www.home-assistant.io/integrations/light.mqtt/#json-schema---configuration
 export interface LightAttribute extends BaseAttribute {
   type: DeviceType.light;
+
+  // Flag that defines if the light works in optimistic mode.
+  optimistic: boolean;
+
+  // Flag that defines if light supports brightness when the rgb, rgbw, or rgbww color mode is supported.
+  brightness?: boolean;
+
+  // Defines the maximum brightness value (i.e., 100%) of the MQTT device.
+  brightness_scale?: number;
+
+  // Flag that defines if the light supports effects.
+  effect?: boolean;
+
+  // The list of effects the light supports.
+  effect_list?: string[];
+
+  // The duration, in seconds, of a “long” flash.
+  flash_time_long?: number;
+
+  // The duration, in seconds, of a “short” flash.
+  flash_time_short?: number;
+
+  // The maximum color temperature in mireds.
+  max_mireds?: number;
+
+  // The minimum color temperature in mireds.
+  min_mireds?: number;
+
+  // A list of color modes supported by the list. Possible color modes are onoff, brightness, color_temp, hs, xy, rgb, rgbw, rgbww, white. 
+  // Note that if onoff or brightness are used, that must be the only value in the list.
+  supported_color_modes?: string[];
+
+  // Defines the maximum white level (i.e., 100%) of the MQTT device. This is used when setting the light to white mode.
+  white_scale?: number;
 }
 
 export interface DeviceTrackerAttribute extends BaseAttribute {
