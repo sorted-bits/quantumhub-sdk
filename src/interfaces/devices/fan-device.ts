2fc1cebee6e301c42d8847f44e094aeaef3cbfdc
Wim Haanstra <wim@haanstra.io>
2024-10-11 06:20:26 +0200

    Fan updates

--- src/interfaces/devices/fan-device.ts
+++ src/interfaces/devices/fan-device.ts
@@ -2,7 +2,9 @@ import { FanAttribute } from "../attributes";
 import { Device } from "./device";
 
 export interface FanDevice extends Device {
-    onFanSpeedChanged(attribute: FanAttribute, value: number): Promise<void>;
+    onFanPowerChanged(attribute: FanAttribute, value: boolean): Promise<void>;
+
+    onFanSpeedChanged?(attribute: FanAttribute, value: number): Promise<void>;
 
     onFanPresetModeChanged?(attribute: FanAttribute, value: string): Promise<void>;
 
